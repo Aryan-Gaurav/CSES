1a4c079d8f3a851a6fa93a0091a9eb69dd23cadd
Aryan Gaurav <aryangaurav0504@gmail.com>
2021-02-01 09:00:37 +0530

    Create 03.Dynamic Range Sum Queries-1.cpp

---
+++ Range Queries/03.Dynamic Range Sum Queries-1.cpp
@@ -0,0 +1,84 @@
+#include <bits/stdc++.h>
+#define lll __int128
+#define ll long long int
+#define ull unsigned long long int
+#define ld long double
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define vi vector<int>
+#define vii vector<pii>
+#define vl vector<ll>
+#define vll vector<pll>
+#define setbits(x) __builtin_popcountll(x)
+#define f(i,l,r) for(ll i=l;i<r;i++)
+#define d(i,n,m) for(ll i=n;i>=m;i--)
+#define deb(...) cout<<" [" <<#__VA_ARGS__ ": " << (__VA_ARGS__) << "] ";
+#define clr(x) memset(x,0,sizeof(x));
+#define all(x) x.begin(),x.end()
+#define rall(x) x.rbegin(),x.rend()
+#define sz(x) (ll)x.size()
+#define P (ll)(1e9+7)
+#define PI acosl(-1.0l)
+#define mod 998244353
+#define pb push_back
+#define F first
+#define S second
+#define IP ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define fixed(n) cout<<std::fixed<<std::setprecision(n) //use for n digits precision of decimal
+/*  freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout);  */
+using namespace std;
+template<class T> bool ckmin(T& a, const T& b) { return b < a ? a = b, 1 : 0; }
+template<class T> bool ckmax(T& a, const T& b) { return a < b ? a = b, 1 : 0; } 
+mt19937 rng(chrono::high_resolution_clock::now().time_since_epoch().count());
+int dx[]={1,0,-1,0};
+int dy[]={0,-1,0,1};
+const int MXN = 2e5+176;
+ll bit[MXN],v[MXN];
+void update(ll i,ll val)
+{
+    while(i<MXN)
+    {
+        bit[i]+=val;
+        i+= i&-i;
+    }
+}
+ll sum(ll i)
+{
+    ll ans=0;
+    while(i>0)
+    {
+        ans+=bit[i];
+        i-=i&-i;
+    }
+    return ans;
+}
+ll sum(ll l,ll r)
+{
+    return sum(r)-sum(l-1);
+}
+int main()
+{
+    IP
+    ll n,q;
+    cin>>n>>q;
+    f(i,1,n+1)
+    {
+        cin>>v[i];
+        update(i,v[i]);
+    }
+    while(q--)
+    {
+        ll op,l,r;
+        cin>>op>>l>>r;
+        if(op==1)
+        {
+            update(l,-v[l]);
+            update(l,r);
+            v[l] = r;
+        }
+        else if(op==2)
+            cout<<sum(l,r)<<"\n";
+    }
+ 
+    return 0;
+}
