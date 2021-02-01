60d8fdf82e6f79e34a6939ac9b9a60d370040f73
Aryan Gaurav <aryangaurav0504@gmail.com>
2021-02-01 09:16:37 +0530

    Create 10.Salary Queries.cpp

---
+++ Range Queries/10.Salary Queries.cpp
@@ -0,0 +1,119 @@
+#include <bits/stdc++.h>
+using namespace std;
+// #include <ext/pb_ds/assoc_container.hpp>    //Read at https://codeforces.com/blog/entry/11080
+// #include <ext/pb_ds/tree_policy.hpp>
+// using namespace __gnu_pbds;
+ 
+// template <typename T> using oset = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+/* 
+find_by_order(k):- returns itertator to kth largest element in set (0-based)
+order_of_key(x):- return number of values strictly less than x
+*/
+#define lll __int128
+#define ll long long int
+#define ull unsigned long long int
+#define ld long double
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define vi vector<int>
+#define vii vector<pii>
+#define vl vector<ll>
+#define vll vector<pll>
+#define ar array
+#define setbits(x) __builtin_popcountll(x)
+#define f(i,l,r) for(ll i=l;i<r;i++)
+#define d(i,n,m) for(ll i=n;i>=m;i--)
+#define deb(...) cout<<" [" <<#__VA_ARGS__ ": " << (__VA_ARGS__) << "] ";
+#define clr(x) memset(x,0,sizeof(x));
+#define all(x) x.begin(),x.end()
+#define rall(x) x.rbegin(),x.rend()
+#define sz(x) (ll)x.size()
+#define P (ll)(1e9+7)
+#define PI acosl(-1.0l)
+#define mod 998244353
+#define pb push_back
+#define F first
+#define S second
+#define IP ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define fixed(n) cout<<std::fixed<<std::setprecision(n) //use for n digits precision of decimal
+/*  freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout);  */
+template<class T> bool ckmin(T& a, const T& b) { return b < a ? a = b, 1 : 0; }
+template<class T> bool ckmax(T& a, const T& b) { return a < b ? a = b, 1 : 0; } 
+mt19937 rng(chrono::high_resolution_clock::now().time_since_epoch().count());
+int dx[]={1,0,-1,0};
+int dy[]={0,-1,0,1};
+const int MXN = 1e6+139;
+ll bit[MXN];
+void update(ll i,ll val)
+{
+    while(i<MXN)
+    {
+        bit[i]+=val;
+        i+=i&-i;
+    }
+}
+ll sum(ll i)
+{
+    ll ans=0;
+    while(i>0)
+    {
+        ans+=bit[i];
+        i-=i&-i;
+    }
+    return ans;
+}
+ll sum(ll l,ll r)
+{
+    return sum(r)-sum(l-1);
+}
+int main()
+{
+    IP
+    ll n,q;
+    cin>>n>>q;
+    vl u(n);
+    set<ll> st;
+    f(i,0,n)
+        cin>>u[i],st.insert(u[i]);
+    vector<ar<ll,3>> query(q);
+    f(i,0,q)
+    {
+        char ch;
+        cin>>ch;
+        auto &[x,y,z]=query[i];
+        x= ch=='!';
+        cin>>y>>z;
+        if(x)
+            st.insert(z);
+    }
+    vl t(all(st));
+    f(i,0,n)
+    {
+        ll idx=lower_bound(all(t),u[i])-t.begin()+1;
+        update(idx,1);
+    }
+    for(auto [x,y,z]:query)
+    {
+        if(x)
+        {
+            y--;    //0-based indexing,be careful
+            ll idx=lower_bound(all(t),u[y])-t.begin()+1;
+            update(idx,-1);
+            idx=lower_bound(all(t),z)-t.begin()+1;
+            update(idx,1);
+            u[y]=z;
+        }
+        else
+        {
+            ll idx1,idx2;
+            idx1=lower_bound(all(t),y)-t.begin()+1;
+            if(binary_search(all(t),z))
+                idx2=lower_bound(all(t),z)-t.begin()+1; //if its found then ok else seek just smaller element
+            else idx2=lower_bound(all(t),z)-t.begin();  
+            // idx2 = upper_bound(all(t),z)-t.begin();  //or it can be directly done like this
+            cout<<sum(idx1,idx2)<<"\n";
+        }
+    }
+    return 0;
+}
+Share code to oth
